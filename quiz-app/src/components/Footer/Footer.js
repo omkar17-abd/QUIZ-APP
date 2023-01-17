9865cda6d85bce361b4975e26285b50bf3fb82ac
omkar17-abd <oskamble_b20@ce.vjti.ac.in>
2023-01-17 23:22:19 +0530

    final commit

---
+++ quiz-app/src/components/Footer/Footer.js
@@ -0,0 +1,18 @@
+const Footer = () => {
+    return (
+      <div
+        style={{
+          textAlign: "center",
+          marginBottom: 10,
+        }}
+      >
+        Made with ♥ by{" "}
+        
+          Omkar Kamble
+        
+      </div>
+    );
+  };
+  
+  export default Footer;
+  
